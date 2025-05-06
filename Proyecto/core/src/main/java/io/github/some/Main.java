3d695924982276bff491668945dbfb827a0c41fc
MrHunter <130599861+MRetnuh@users.noreply.github.com>
2025-05-06 18:36:44 -0300

    Add files via upload

--- Proyecto/core/src/main/java/io/github/some/Main.java
+++ Proyecto/core/src/main/java/io/github/some/Main.java
@@ -2,38 +2,34 @@ package io.github.some;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-   private Music music;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private OrthographicCamera camera;
+
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("akame_de_frente_(detenida).png");
-        music = Gdx.audio.newMusic(Gdx.files.internal("primeraisla.mp3"));
-        music.setLooping(true);   // Repetir en bucle
-        music.setVolume(0.5f);    // Volumen (0.0 a 1.0)
-        music.play();        
+        map = new TmxMapLoader().load("sample.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        camera.update();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        map.dispose();
+        mapRenderer.dispose();
     }
 }
