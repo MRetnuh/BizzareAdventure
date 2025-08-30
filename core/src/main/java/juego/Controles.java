8034c7fa9793aa65d224773dc14deb3435929348
Kevin de Groote <kevindegro@gmail.com>
2025-08-30 13:12:45 -0300

    Cambio en Controles con KeyListener

--- core/src/main/java/juego/Controles.java
+++ core/src/main/java/juego/Controles.java
@@ -1,106 +1,132 @@
 package juego;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Keys;
 
-import audios.Musica;
-import estilos.EstiloTexto;
-import io.github.some.Principal;
 
-public class Controles implements Screen {
-	private Screen screenAnterior;
-    private final Principal JUEGO;
-    private Stage stage;
-    private Skin skin;
-    private Musica musicaControles;
+public class Controles implements InputProcessor {
+	
+    // Jugador 1
+    private boolean w, a, s, d, m, p;
+    private boolean arriba, izquierda, abajo, derecha, atacar, pausa;
+
+    // Jugador 2
+    private boolean up, left, down, right, espacio, enter;
+    private boolean arriba2, izquierda2, abajo2, derecha2, atacar2, pausa2;
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            // Jugador 1
+            case Keys.W: w = true; arriba = true; break;
+            case Keys.A: a = true; izquierda = true; break;
+            case Keys.S: s = true; abajo = true; break;
+            case Keys.D: d = true; derecha = true; break;
+            case Keys.M: m = true; atacar = true; break;
+            case Keys.P: p = true; pausa = true; break;
+
+            // Jugador 2
+            case Keys.UP:    up = true; arriba2 = true; break;
+            case Keys.LEFT:  left = true; izquierda2 = true; break;
+            case Keys.DOWN:  down = true; abajo2 = true; break;
+            case Keys.RIGHT: right = true; derecha2 = true; break;
+            case Keys.SPACE: espacio = true; atacar2 = true; break;
+            case Keys.ENTER: enter = true; pausa2 = true; break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        switch (keycode) {
+            // Jugador 1
+            case Keys.W: w = false; break;
+            case Keys.A: a = false; break;
+            case Keys.S: s = false; break;
+            case Keys.D: d = false; break;
+            case Keys.M: m = false; break;
+            case Keys.P: p = false; break;
+
+            // Jugador 2
+            case Keys.UP:    arriba = false; break;
+            case Keys.LEFT:  izquierda = false; break;
+            case Keys.DOWN:  abajo = false; break;
+            case Keys.RIGHT: derecha = false; break;
+            case Keys.SPACE: espacio = false; break;
+            case Keys.ENTER: enter = false; break;
+        }
+        return true;
+    }
+
+    @Override public boolean keyTyped(char character) { 
+    	return false; 
+    }
     
-    public Controles(Principal juego,  Screen screenAnterior) {
-        this.JUEGO = juego;
-        this.musicaControles = juego.getMusica(); 
-        this.screenAnterior = screenAnterior;
+    @Override public boolean touchDown(int x, int y, int pointer, int button) {
+    	return false; 
+    }
+    
+    @Override public boolean touchUp(int x, int y, int pointer, int button) { 
+    	return false; 
+    }
+    
+    @Override public boolean touchCancelled(int x, int y, int pointer, int button) { 
+    	return false; 
+    }
+    
+    @Override public boolean touchDragged(int x, int y, int pointer) { 
+    	return false; 
+    }
+    
+    @Override public boolean mouseMoved(int x, int y) { 
+    	return false; 
+    }
+    
+    @Override public boolean scrolled(float amountX, float amountY) {
+    	return false; 
     }
 
-    @Override
-    public void show() {
-    	this.stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(this.stage);
-
-        this.skin = new Skin(Gdx.files.internal("uiskin.json"));
-
-        Label titulo = new Label("CONTROLES", EstiloTexto.ponerEstiloLabel(70, Color.WHITE));
-        titulo.setAlignment(Align.center);
-
-        Label movimientoTitulo = new Label("Movimiento:", EstiloTexto.ponerEstiloLabel(60, Color.ORANGE));
-        Label combateTitulo = new Label("Combate:", EstiloTexto.ponerEstiloLabel(60, Color.ORANGE));
-        Label opcionesTitulo = new Label("Opciones:", EstiloTexto.ponerEstiloLabel(60, Color.ORANGE));
-
-        Label w = new Label("W: Saltar", EstiloTexto.ponerEstiloLabel(48, Color.LIGHT_GRAY));
-        Label a = new Label("A: Moverse a la izquierda", EstiloTexto.ponerEstiloLabel(48, Color.LIGHT_GRAY));
-        Label d = new Label("D: Moverse a la derecha", EstiloTexto.ponerEstiloLabel(48, Color.LIGHT_GRAY));
-        Label s = new Label("S: Agacharse", EstiloTexto.ponerEstiloLabel(48, Color.LIGHT_GRAY));
-
-        Label m = new Label("M: Atacar", EstiloTexto.ponerEstiloLabel(48, Color.LIGHT_GRAY));
-
-        Label p = new Label("P: Pausar / Opciones", EstiloTexto.ponerEstiloLabel(48, Color.LIGHT_GRAY));
-
-        TextButton volverBtn = new TextButton("Volver", EstiloTexto.ponerEstiloBoton(skin, 48, Color.RED));
-        volverBtn.addListener(event -> {
-            if (Gdx.input.isTouched()) {
-            	this.JUEGO.setScreen(new Opciones(this.JUEGO, this.screenAnterior));
-            }
-            return true;
-        });
-
-        Table tabla = new Table();
-        tabla.setFillParent(true);
-        tabla.top().padTop(30);
-
-        tabla.add(titulo).colspan(1).padBottom(40).row();
-
-        tabla.add(movimientoTitulo).left().padBottom(10).row();
-        tabla.add(w).left().padBottom(5).row();
-        tabla.add(a).left().padBottom(5).row();
-        tabla.add(d).left().padBottom(5).row();
-        tabla.add(s).left().padBottom(25).row();
-
-        tabla.add(combateTitulo).left().padBottom(10).row();
-        tabla.add(m).left().padBottom(25).row();
-
-        tabla.add(opcionesTitulo).left().padBottom(10).row();
-        tabla.add(p).left().padBottom(40).row();
-
-        tabla.add(volverBtn).center().size(200, 60);
-
-        this.stage.addActor(tabla);
+    public boolean W() { 
+    	return w; 
+    }
+    public boolean A() { 
+    	return a; 
+    }
+    public boolean S() { 
+    	return s; 
+    }
+    public boolean D() { 
+    	return d; 
+    }
+    public boolean M() { 
+    	return m; 
+    }
+    public boolean P() { 
+    	return p; 
     }
 
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        this.stage.act(delta);
-        this.stage.draw();
+  
+    public boolean arriba() { 
+    	return up; 
     }
-
-    @Override public void resize(int width, int height) {}
-    @Override public void pause() {}
-    @Override public void resume() {}
-    @Override public void hide() {}
-
-    @Override
-    public void dispose() {
-    	this.stage.dispose();
-    	this.skin.dispose();
+    
+    public boolean izquierda() { 
+    	return left; 
     }
+    public boolean abajo() { 
+    	return down; 
+    }
+    
+    public boolean derecha() { 
+    	return right; 
+    }
+    
+    public boolean espacio() { 
+    	return space; 
+    }
+    
+    public boolean enter() { 
+    	return enter; 
+    }
+   
 }
