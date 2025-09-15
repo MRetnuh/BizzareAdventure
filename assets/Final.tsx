e0c13236ef192cbd95a0de2b88923e22ec8baf30
MejiaEynar <eynarmejia03@gmail.com>
2025-09-15 15:54:06 -0300

    insercion de nuevos assets en el mapa

---
+++ assets/Final.tsx
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Final" tilewidth="32" tileheight="32" tilecount="144" columns="18">
+ <image source="final.png" trans="ff00ff" width="576" height="256"/>
+ <tile id="0" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3.6875" width="32" height="28.3125"/>
+  </objectgroup>
+ </tile>
+ <tile id="1" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="4" width="31" height="28"/>
+  </objectgroup>
+ </tile>
+ <tile id="2" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="5" width="31" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="32" height="28"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="17" width="32" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16" width="31" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="19" width="22" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="18" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="19" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="20" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="21" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="22" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="23" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="31"/>
+   <object id="2" x="0" y="1" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="24" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="9" width="2" height="23"/>
+  </objectgroup>
+ </tile>
+ <tile id="36" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="37" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="38" type="Final"/>
+ <tile id="39" type="Final"/>
+ <tile id="40" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="1" width="30" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="41" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="2" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="42" type="Final">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="2" height="28"/>
+  </objectgroup>
+ </tile>
+</tileset>
